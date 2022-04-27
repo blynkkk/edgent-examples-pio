8a30dda2b1407dfb5755188362583dfe19bd786c
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2022-04-27 12:36:24 +0300

    Add SDL2 emulator, improve navigation

--- PIO_Edgent_Wio_Terminal/src/Edgent_Wio_Terminal.ino
+++ PIO_Edgent_Wio_Terminal/src/Edgent_Wio_Terminal.ino
@@ -18,17 +18,40 @@ void display_update_state();
 void display_run();
 
 #include "BlynkEdgent.h"
-
 #include <lvgl.h>
 #include <TFT_eSPI.h>
 #include "lcd_backlight.hpp"
 
 /*
- * Buzzer
+ * Basic IO
  */
 
-void buzzer_init() {
+const int BTN_UP        = WIO_5S_UP;
+const int BTN_DOWN      = WIO_5S_DOWN;
+const int BTN_LEFT      = WIO_5S_LEFT;
+const int BTN_RIGHT     = WIO_5S_RIGHT;
+const int BTN_OK        = WIO_5S_PRESS;
+const int BTN_A         = WIO_KEY_A;
+const int BTN_B         = WIO_KEY_B;
+const int BTN_C         = WIO_KEY_C;
+
+LCDBackLight backLight;
+
+void board_init() {
+    backLight.initialize();
+    backLight.setBrightness(backLight.getMaxBrightness()/2);
+
     pinMode(WIO_BUZZER, OUTPUT);
+
+    /* Init keypad */
+    pinMode(BTN_UP,    INPUT_PULLUP);
+    pinMode(BTN_DOWN,  INPUT_PULLUP);
+    pinMode(BTN_LEFT,  INPUT_PULLUP);
+    pinMode(BTN_RIGHT, INPUT_PULLUP);
+    pinMode(BTN_OK,    INPUT_PULLUP);
+    pinMode(BTN_A,     INPUT_PULLUP);
+    pinMode(BTN_B,     INPUT_PULLUP);
+    pinMode(BTN_C,     INPUT_PULLUP);
 }
 
 void playTone(int tone, int duration) {
@@ -44,169 +67,9 @@ void playTone(int tone, int duration) {
  * LVGL configuration
  */
 
-const uint16_t SCREEN_WIDTH  = 320;
-const uint16_t SCREEN_HEIGHT = 240;
+#include <GUI.h>
 
-const int BTN_UP        = WIO_5S_UP;
-const int BTN_DOWN      = WIO_5S_DOWN;
-const int BTN_LEFT      = WIO_5S_LEFT;
-const int BTN_RIGHT     = WIO_5S_RIGHT;
-const int BTN_OK        = WIO_5S_PRESS;
-const int BTN_A         = WIO_KEY_A;
-const int BTN_B         = WIO_KEY_B;
-const int BTN_C         = WIO_KEY_C;
-
-
-static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[ SCREEN_WIDTH * 16 ];
-
-TFT_eSPI tft = TFT_eSPI(SCREEN_WIDTH, SCREEN_HEIGHT); /* TFT instance */
-LCDBackLight backLight;
-
-lv_obj_t *lbl_state, *lbl_rssi, *lbl_ssid, *lbl_ip, *lbl_mac, *btn_reset;
-
-const char* STR_NONE = "---";
-
-static lv_obj_t * create_text(lv_obj_t * parent, const char * icon, const char * txt)
-{
-    lv_obj_t * obj = lv_menu_cont_create(parent);
-
-    if (icon) {
-        lv_obj_t * img = lv_img_create(obj);
-        lv_img_set_src(img, icon);
-    }
-
-    if (txt) {
-        lv_obj_t * label = lv_label_create(obj);
-        lv_label_set_text(label, txt);
-        lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
-        lv_obj_set_flex_grow(label, 1);
-    }
-
-    return obj;
-}
-
-static lv_obj_t * create_slider(lv_obj_t * parent, const char * icon, const char * txt, int32_t min, int32_t max, int32_t val)
-{
-    lv_obj_t * obj = create_text(parent, icon, txt);
-
-    lv_obj_t * slider = lv_slider_create(obj);
-    lv_obj_set_flex_grow(slider, 1);
-    lv_slider_set_range(slider, min, max);
-    lv_slider_set_value(slider, val, LV_ANIM_OFF);
-
-    if (icon == NULL) {
-        //lv_obj_add_flag(slider, LV_OBJ_FLAG_FLEX_IN_NEW_TRACK);
-    }
-
-    return obj;
-}
-
-static lv_obj_t * create_switch(lv_obj_t * parent, const char * icon, const char * txt, bool chk)
-{
-    lv_obj_t * obj = create_text(parent, icon, txt);
-
-    lv_obj_t * sw = lv_switch_create(obj);
-    lv_obj_add_state(sw, chk ? LV_STATE_CHECKED : 0);
-
-    return obj;
-}
-
-static lv_obj_t * create_status(lv_obj_t * parent, const char * icon, const char * txt, const char * txt2)
-{
-    lv_obj_t * obj = create_text(parent, icon, txt);
-
-    lv_obj_t * label = lv_label_create(obj);
-    lv_label_set_text(label, txt2);
-    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
-
-    return obj;
-}
-
-static lv_obj_t * create_link(lv_obj_t * parent, const char * icon, const char * txt, const char * txt2)
-{
-    lv_obj_t * obj = create_text(parent, icon, txt);
-
-    lv_obj_t * label = lv_label_create(obj);
-    lv_label_set_text(label, txt2);
-    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
-
-    lv_obj_add_flag(label, LV_OBJ_FLAG_CLICKABLE);
-    lv_group_add_obj(lv_group_get_default(), label);
-
-    lv_obj_set_style_text_color(label, lv_palette_main(LV_PALETTE_BLUE), LV_STATE_DEFAULT);
-    lv_obj_set_style_text_decor(label, LV_TEXT_DECOR_UNDERLINE, LV_STATE_FOCUSED);
-    lv_obj_set_style_text_decor(label, LV_TEXT_DECOR_UNDERLINE, LV_STATE_PRESSED);
-
-    return obj;
-}
-
-static lv_obj_t * create_button(lv_obj_t * parent, const char * icon, const char * txt, const char * txt2)
-{
-    lv_obj_t * obj = create_text(parent, icon, txt);
-
-    lv_obj_t * btn = lv_btn_create(obj);
-    
-    lv_obj_t * label = lv_label_create(btn);
-    lv_label_set_text(label, txt2);
-    lv_obj_center(label);
-
-    return obj;
-}
-
-static void msgbox_reset_clicked(lv_event_t* e)
-{
-    lv_obj_t * mbox = lv_event_get_current_target(e);
-    if (lv_msgbox_get_active_btn(mbox) == 0) {
-        if (BlynkState::get() != MODE_WAIT_CONFIG) {
-            BlynkState::set(MODE_RESET_CONFIG);
-        }
-    }
-    lv_msgbox_close(mbox);
-}
-
-static void btn_reset_clicked(lv_event_t* e)
-{
-    static const char * btns[] = { "OK", "Cancel", NULL };
-
-    lv_obj_t * mbox = lv_msgbox_create(NULL, "Warning", "Device configuration will be reset", btns, false);
-    lv_obj_add_event_cb(mbox, msgbox_reset_clicked, LV_EVENT_VALUE_CHANGED, NULL);
-    lv_obj_center(mbox);
-
-    lv_btnmatrix_set_selected_btn(lv_msgbox_get_btns(mbox), 1);
-    lv_group_focus_obj(lv_msgbox_get_btns(mbox));
-}
-
-static void brightness_changed(lv_event_t* e)
-{
-    lv_obj_t * slider = lv_event_get_target(e);
-    backLight.setBrightness(lv_slider_get_value(slider));
-}
-
-bool soundEnabled = true;
-
-static void sound_switched(lv_event_t* e)
-{
-    lv_obj_t * sw = lv_event_get_target(e);
-    soundEnabled = lv_obj_has_state(sw, LV_STATE_CHECKED);
-}
-
-static void group_focus_cb(lv_group_t* g)
-{
-    if (soundEnabled) {
-        playTone(1915, 10);
-    }
-}
-
-static void menu_opened(lv_event_t* e)
-{
-    // TODO: Does not always focus the back button. LVGL bug?
-    lv_obj_t * menu = (lv_obj_t *)lv_event_get_user_data(e);
-    lv_obj_t * btn = lv_menu_get_main_header_back_btn(menu);
-    if (!lv_menu_back_btn_is_root(menu, btn)) {
-        lv_group_focus_obj(btn);
-    }
-}
+TFT_eSPI tft = TFT_eSPI(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 void display_flush(lv_disp_drv_t* disp, const lv_area_t* area, lv_color_t* color_p)
 {
@@ -247,11 +110,8 @@ void display_init()
     tft.begin();
     tft.setRotation(3);
 
-    backLight.initialize();
-    backLight.setBrightness(backLight.getMaxBrightness()/2);
-
     lv_init();
-    lv_disp_draw_buf_init(&draw_buf, buf, NULL, SCREEN_WIDTH * 16);
+    lv_disp_draw_buf_init(&draw_buf, frame_buf, NULL, SCREEN_WIDTH * 16);
 
     /* Init the display */
     static lv_disp_drv_t disp_drv;
@@ -261,106 +121,18 @@ void display_init()
     disp_drv.flush_cb = display_flush;
     disp_drv.draw_buf = &draw_buf;
     lv_disp_drv_register(&disp_drv);
-    
-    /* Init keypad */
-    pinMode(BTN_UP,    INPUT_PULLUP);
-    pinMode(BTN_DOWN,  INPUT_PULLUP);
-    pinMode(BTN_LEFT,  INPUT_PULLUP);
-    pinMode(BTN_RIGHT, INPUT_PULLUP);
-    pinMode(BTN_OK,    INPUT_PULLUP);
-    pinMode(BTN_A,     INPUT_PULLUP);
-    pinMode(BTN_B,     INPUT_PULLUP);
-    pinMode(BTN_C,     INPUT_PULLUP);
 
     static lv_indev_drv_t kb_drv;
     lv_indev_drv_init(&kb_drv);
     kb_drv.type = LV_INDEV_TYPE_KEYPAD;
     kb_drv.read_cb = keyboard_read;
     lv_indev_t* kb_indev = lv_indev_drv_register(&kb_drv);
-    
+
     /* Default group */
     lv_group_t * g = lv_group_create();
     lv_group_set_default(g);
     lv_group_set_focus_cb(g, group_focus_cb);
     lv_indev_set_group(kb_indev, g);
-
-    /* Create simple Menu */
-
-    lv_obj_t * menu = lv_menu_create(lv_scr_act());
-    lv_obj_add_event_cb(menu, menu_opened, LV_EVENT_VALUE_CHANGED, menu);
-
-    lv_color_t bg_color = lv_obj_get_style_bg_color(menu, 0);
-    lv_obj_set_style_bg_color(menu, lv_color_darken(lv_obj_get_style_bg_color(menu, 0), 40), 0);
-    lv_obj_set_size(menu, lv_disp_get_hor_res(NULL), lv_disp_get_ver_res(NULL));
-    lv_obj_center(menu);
-
-    lv_obj_t * cont;
-    lv_obj_t * section;
-    
-    lv_obj_t * sub_about_page = lv_menu_page_create(menu, NULL);
-    lv_obj_set_style_pad_hor(sub_about_page, lv_obj_get_style_pad_left(lv_menu_get_main_header(menu), 0), 0);
-    lv_menu_separator_create(sub_about_page);
-    section = lv_menu_section_create(sub_about_page);
-
-    String lv_ver = String("v") + lv_version_major() + "." +
-                    lv_version_minor() + "." + lv_version_patch();
-                    
-    String wifi_ver = String("v") + rpc_system_version();
-
-    create_status(section, NULL, "Firmware", "v" BLYNK_FIRMWARE_VERSION);
-    create_status(section, NULL, "Build", __DATE__ " " __TIME__);
-    create_status(section, NULL, "Blynk", "v" BLYNK_VERSION);
-    create_status(section, NULL, "LVGL", lv_ver.c_str());
-    create_status(section, NULL, "WiFi FW", wifi_ver.c_str());
-
-    lv_obj_t * sub_status_page = lv_menu_page_create(menu, NULL);
-    lv_obj_set_style_pad_hor(sub_status_page, lv_obj_get_style_pad_left(lv_menu_get_main_header(menu), 0), 0);
-    lv_menu_separator_create(sub_status_page);
-    section = lv_menu_section_create(sub_status_page);
-    
-    cont = create_status(section, NULL, "Network SSID", STR_NONE);
-    lbl_ssid = lv_obj_get_child(cont, 1);
-    
-    cont = create_status(section, NULL, "IP address", STR_NONE);
-    lbl_ip = lv_obj_get_child(cont, 1);
-
-    cont = create_status(section, NULL, "RSSI", STR_NONE);
-    lbl_rssi = lv_obj_get_child(cont, 1);
-
-    cont = create_status(section, NULL, "MAC", STR_NONE);
-    lbl_mac = lv_obj_get_child(cont, 1);
-    
-    lv_menu_separator_create(sub_status_page);
-    section = lv_menu_section_create(sub_status_page);
-    cont = create_button(section, NULL, "Configuration", "Reset");
-    btn_reset = lv_obj_get_child(cont, 1);
-    lv_obj_add_event_cb(btn_reset, btn_reset_clicked, LV_EVENT_CLICKED, NULL);
-
-    /* Create a root page */
-    static char devName[64];
-    getWiFiName(devName, sizeof(devName));
-    
-    lv_obj_t * root_page = lv_menu_page_create(menu, devName);
-    lv_obj_set_style_pad_hor(root_page, lv_obj_get_style_pad_left(lv_menu_get_main_header(menu), 0), 0);
-    section = lv_menu_section_create(root_page);
-
-    cont = create_slider(section, LV_SYMBOL_IMAGE, "Brightness", 1, backLight.getMaxBrightness(), backLight.getBrightness());
-    lv_obj_add_event_cb(lv_obj_get_child(cont, 2), brightness_changed, LV_EVENT_VALUE_CHANGED, NULL);
-
-    cont = create_switch(section, LV_SYMBOL_AUDIO, "Sound", soundEnabled);
-    lv_obj_add_event_cb(lv_obj_get_child(cont, 2), sound_switched, LV_EVENT_VALUE_CHANGED, NULL);
-    
-    cont = create_link(section, LV_SYMBOL_WIFI, "Status", " ");
-    lbl_state = lv_obj_get_child(cont, 2);
-    lv_menu_set_load_page_event(menu, lbl_state, sub_status_page);
-
-    lv_menu_separator_create(root_page);
-    section = lv_menu_section_create(root_page);
-    cont = create_link(section, LV_SYMBOL_SETTINGS, "About", ">");
-    cont = lv_obj_get_child(cont, 2);
-    lv_menu_set_load_page_event(menu, cont, sub_about_page);
-
-    lv_menu_set_page(menu, root_page);
 }
 
 void display_update_state()
@@ -405,8 +177,9 @@ void setup()
   Serial.begin(115200);
   delay(100);
 
+  board_init();
   display_init();
-  buzzer_init();
+  gui_init();
 
   edgentTimer.setInterval(3000, []() {
     String mac = WiFi.macAddress();
