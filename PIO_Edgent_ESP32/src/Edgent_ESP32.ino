fd39f2b69febd7fe34024e5d69614f31546358f7
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2023-01-26 09:36:38 +0200

    Update examples

--- PIO_Edgent_ESP32/src/Edgent_ESP32.ino
+++ PIO_Edgent_ESP32/src/Edgent_ESP32.ino
@@ -1,5 +1,24 @@
+/*************************************************************
+  Blynk is a platform with iOS and Android apps to control
+  ESP32, Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build mobile and web interfaces for any
+  projects by simply dragging and dropping widgets.
+    Downloads, docs, tutorials: https://www.blynk.io
+    Sketch generator:           https://examples.blynk.cc
+    Blynk community:            https://community.blynk.cc
+    Follow us:                  https://www.fb.com/blynkapp
+                                https://twitter.com/blynk_app
 
-// Fill-in information from your Blynk Template here
+  Blynk library is licensed under MIT license
+ *************************************************************
+  Blynk.Edgent implements:
+  - Blynk.Inject - Dynamic WiFi credentials provisioning
+  - Blynk.Air    - Over The Air firmware updates
+  - Device state indication using a physical LED
+  - Credentials reset using a physical Button
+ *************************************************************/
+
+/* Fill in information from your Blynk Template here */
 //#define BLYNK_TEMPLATE_ID           "TMPxxxxxx"
 //#define BLYNK_TEMPLATE_NAME         "Device"
 
--- PIO_Edgent_Wio_Terminal/src/Edgent_Wio_Terminal.ino
+++ PIO_Edgent_Wio_Terminal/src/Edgent_Wio_Terminal.ino
@@ -1,9 +1,26 @@
-/*
- * Please update the WiFi module firmware:
- *   https://wiki.seeedstudio.com/Wio-Terminal-Network-Overview
- */
+/*************************************************************
+  Blynk is a platform with iOS and Android apps to control
+  ESP32, Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build mobile and web interfaces for any
+  projects by simply dragging and dropping widgets.
+    Downloads, docs, tutorials: https://www.blynk.io
+    Sketch generator:           https://examples.blynk.cc
+    Blynk community:            https://community.blynk.cc
+    Follow us:                  https://www.fb.com/blynkapp
+                                https://twitter.com/blynk_app
 
-// Fill-in information from your Blynk Template here
+  Blynk library is licensed under MIT license
+ *************************************************************
+  Blynk.Edgent implements:
+  - Blynk.Inject - Dynamic WiFi credentials provisioning
+  - Blynk.Air    - Over The Air firmware updates
+  - Simple LVGL-based UI
+
+  NOTE: Please update the WiFi module firmware, if needed:
+    https://wiki.seeedstudio.com/Wio-Terminal-Network-Overview
+ *************************************************************/
+
+/* Fill in information from your Blynk Template here */
 //#define BLYNK_TEMPLATE_ID           "TMPxxxxxxx"
 //#define BLYNK_TEMPLATE_NAME         "Device"
 
